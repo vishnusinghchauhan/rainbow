172302427d39814c0d53ef3272152af5697b2572
Vishnu Chauhan <vsvishnusingh8@gmail.com>
2024-02-20 10:06:13 +0530

    baisc page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React test
-        </a>
-      </header>
+    <header id="site-header" class="fixed-top">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+               <img src="assets/images/logo.jpeg" style={{width:"100px"}}/>
+                <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon fa icon-expand fa-bars"></span>
+                    <span class="navbar-toggler-icon fa icon-close fa-times"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="courses.html">Courses</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                    <div class="header-search position-relative">
+                        <div class="search-right mx-lg-2">
+                            <a href="#search" class="btn search-btn p-0" title="search">
+                                <i class="fas fa-search"></i></a>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="cont-ser-position">
+                    <nav class="navigation">
+                        <div class="theme-switch-wrapper">
+                            <label class="theme-switch" for="checkbox">
+                                <input type="checkbox" id="checkbox" />
+                                <div class="mode-container">
+                                    <i class="gg-sun"></i>
+                                    <i class="gg-moon"></i>
+                                </div>
+                            </label>
+                        </div>
+                    </nav>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    <section id="home" class="w3l-banner py-5">
+        <div class="banner-content">
+            <div class="container py-4">
+                <div class="row align-items-center pt-sm-5 pt-4">
+                    <div class="col-md-6">
+                        <h3 class="mb-lg-4 mb-3">Your Kids Deserve The<span class="d-block">Best Education</span>
+                        </h3>
+                        <p class="banner-sub">Active Learning, Expert Teachers & Safe Environment</p>
+                        <div class="d-flex align-items-center buttons-banner">
+                            <a href="contact.html" class="btn btn-style mt-lg-5 mt-4">Admission Now</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 right-banner-2 text-end position-relative mt-md-0 mt-5">
+                        <div class="sub-banner-image mx-auto">
+                            <img src="assets/images/banner.png" class="img-fluid position-relative" alt=" " />
+                        </div>
+                        <div class="banner-style-1 position-absolute">
+                            <div class="banner-style-2 position-relative">
+                                <h4>Back to School</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+ <section class="services-w3l-block py-5" id="features">
+        <div class="container py-md-5 py-4">
+            <div class="title-main text-center mx-auto mb-md-5 mb-4" >
+                <p class="text-uppercase">Best Features</p>
+                <h3 class="title-style">Achieve Your Goals With Edu School</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch">
+                    <div class="icon-box icon-box-clr-1">
+                        <div class="icon"><i class="fas fa-user-friends"></i></div>
+                        <h4 class="title"><a href="#about">Expert Teachers</a></h4>
+                        <p>Ras effic itur metusga via suscipit consect eturerse adi unde omnis.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mt-md-0 mt-4">
+                    <div class="icon-box icon-box-clr-2">
+                        <div class="icon"><i class="fas fa-book-reader"></i></div>
+                        <h4 class="title"><a href="#about">Quality Education</a></h4>
+                        <p>Ras effic itur metusga via suscipit consect eturerse adi unde omnis.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box icon-box-clr-3">
+                        <div class="icon"><i class="fas fa-user-graduate"></i></div>
+                        <h4 class="title"><a href="#about">Life Time Support</a></h4>
+                        <p>Ras effic itur metusga via suscipit consect eturerse adi unde omnis.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mt-lg-0 mt-4">
+                    <div class="icon-box icon-box-clr-4">
+                        <div class="icon"><i class="fas fa-university"></i></div>
+                        <h4 class="title"><a href="#about">Best Scholarships</a></h4>
+                        <p>Ras effic itur metusga via suscipit consect eturerse adi unde omnis.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+   
+
+    <footer class="w3l-footer-29-main">
+        <div class="footer-29 pt-5 pb-4">
+            <div class="container pt-md-4">
+                <div class="row footer-top-29">
+                    <div class="col-lg-4 col-md-6 footer-list-29">
+                        <h6 class="footer-title-29">Contact Info </h6>
+                        <p class="mb-2 pe-xl-5">Address : Edu School, 10001, 5th Avenue, #06 lane street, NY - 62617.
+                        </p>
+                        <p class="mb-2">Phone Number : <a href="tel:+1(21) 234 4567">+1(21) 234 4567</a></p>
+                        <p class="mb-2">Email : <a href="mailto:info@example.com">info@example.com</a></p>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-6 footer-list-29 mt-md-0 mt-4">
+                        <ul>
+                            <h6 class="footer-title-29">Quick Links</h6>
+                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="courses.html">Courses</a></li>
+                            <li><a href="#become-teacher">Become a Teacher</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                            <li><a href="#career">Career</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-2 col-md-3 col-6 ps-lg-5 ps-lg-4 footer-list-29 mt-md-0 mt-4">
+                        <ul>
+                            <h6 class="footer-title-29">Explore</h6>
+                            <li><a href="#blog">Blog Posts</a></li>
+                            <li><a href="#privacy">Privacy policy</a></li>
+                            <li><a href="contact.html">Contact Us</a></li>
+                            <li><a href="#license">License & uses</a></li>
+                            <li><a href="#tutorials">Tutorials</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-8 footer-list-29 mt-lg-0 mt-4 ps-lg-5">
+                        <h6 class="footer-title-29">Subscribe</h6>
+                        <form action="#" class="subscribe d-flex" method="post">
+                            <input type="email" name="email" placeholder="Email Address" required="" />
+                            <button class="button-style"><span class="fa fa-paper-plane"
+                                    aria-hidden="true"></span></button>
+                        </form>
+                        <p class="mt-3">Subscribe to our mailing list and get updates to your email inbox.</p>
+                    </div>
+                </div>
+                <p class="copy-footer-29 text-center pt-lg-2 mt-5 pb-2">© 2021 Edu School. All rights reserved. Design
+                    by <a href="https://w3layouts.com/" target="_blank">W3Layouts</a></p>
+            </div>
+        </div>
+    </footer>
     </div>
   );
 }
